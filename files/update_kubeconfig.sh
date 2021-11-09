f3d7f5baca1404b0906c68709d912bd82d30969f
David G. Pullman <dpullman@emergent360.com>
2021-11-09 09:26:27 -0500

    Update the kubeconfig job

--- files/update_kubeconfig.sh
+++ files/update_kubeconfig.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-CLUSTER_NAME=$1
-BUILD_PATH="/home/ec2-user/$CLUSTER_NAME/build"
+LE_DOMAIN=$1
+BUILD_PATH=$2
 
 
-cat "$BUILD_PATH/auth/kubeconfig" | grep certificate-authority-data | awk '{print $2}' | base64 -d > "$BUILD_PATH/auth/kubeconfig-bundle.crt"
-cat /home/ec2-user/.certbot/config/archive/api."$CLUSTER_NAME".lab-emergent360.com/chain1.pem >> "$BUILD_PATH/auth/kubeconfig-bundle.crt"
-OLD_CERTS=$(cat "$BUILD_PATH/auth/kubeconfig" | grep certificate-authority-data | awk '{print $2}' | tr -d '\n')
-NEW_CERTS=$(cat $BUILD_PATH/auth/kubeconfig-bundle.crt | base64 -w 0 | tr -d '\n')
-sed -i "s|$OLD_CERTS|$NEW_CERTS|" "$BUILD_PATH/auth/kubeconfig"
+cat "${BUILD_PATH}/auth/kubeconfig" | grep certificate-authority-data | awk '{print $2}' | base64 -d > "${BUILD_PATH}/auth/kubeconfig-bundle.crt"
+cat "/home/ec2-user/.certbot/config/archive/api.${LE_DOMAIN}.lab-emergent360.com/chain1.pem" >> "${BUILD_PATH}/auth/kubeconfig-bundle.crt"
+OLD_CERTS=$(cat "${BUILD_PATH}/auth/kubeconfig" | grep certificate-authority-data | awk '{print $2}' | tr -d '\n')
+NEW_CERTS=$(cat "${BUILD_PATH}/auth/kubeconfig-bundle.crt" | base64 -w 0 | tr -d '\n')
+sed -i "s|${OLD_CERTS}|${NEW_CERTS}|" "${BUILD_PATH}/auth/kubeconfig"
